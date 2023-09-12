9cb990ceba6bd39994608c69415dc73457d20da7
THIEUXK <116102779+THIEUXK@users.noreply.github.com>
2023-09-13 00:28:00 +0700

    13/9/12:27

---
+++ MCV/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿namespace MCV.Models
+{
+    public class Account
+    {
+        public Guid id { get; set; }
+        public Guid? RoleId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Avatar { get; set; }
+        public bool Status { get; set; }
+
+        public Role? Role { get; set; }
+    }
+}
---
+++ MCV/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace MCV.Models
+{
+    public class Address
+    {
+        public Guid id { get; set; }
+        public Guid? AccountId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string SpecificAddress { get; set; }
+        public string Ward { get; set; }
+        public string District { get; set; }
+        public string Province { get; set; }
+        public string Note { get; set; }
+        
+    }
+}
